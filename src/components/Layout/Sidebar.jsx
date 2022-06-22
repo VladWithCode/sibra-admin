b8ae47a2210c10e4c34a8ef16b9a4c1e14c59d17
vladwithcode <vladwithcode@gmail.com>
2022-06-22 11:50:23 -0500

    Icon container & home link

--- src/components/Layout/Sidebar.jsx
+++ src/components/Layout/Sidebar.jsx
@@ -1,10 +1,21 @@
 import React, { useContext } from 'react';
 import SidebarContext from '../../context/Sidebar/SidebarContext';
+import NavLink from './NavLink';
 
 function Sidebar() {
   const { isExpanded } = useContext(SidebarContext);
 
-  return <nav className={isExpanded}></nav>;
+  return (
+    <nav className={isExpanded ? 'expand' : ''}>
+      <div className='icons-container'>
+        <NavLink to='/'>
+          <svg>
+            <use href='/assets/svg/home.svg#home'></use>
+          </svg>
+        </NavLink>
+      </div>
+    </nav>
+  );
 }
 
 export default Sidebar;
