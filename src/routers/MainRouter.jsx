b122c3f08f9dfc28afc9428b5632f009e9d7502e
vladwithcode <vladwithcode@gmail.com>
2022-06-22 15:51:52 -0500

    Project Screen

--- src/routers/MainRouter.jsx
+++ src/routers/MainRouter.jsx
@@ -2,6 +2,7 @@ import { HashRouter, Route, Routes } from 'react-router-dom';
 import { App } from '../App';
 import Error from '../screens/Error';
 import Home from '../screens/Home';
+import Projects from '../screens/Projects/Projects';
 
 function MainRouter() {
   return (
@@ -9,6 +10,7 @@ function MainRouter() {
       <Routes>
         <Route path='/' element={<App />}>
           <Route index element={<Home />} />
+          <Route path='/projects' element={<Projects />} />
           <Route path='*' element={<Error />} />
         </Route>
       </Routes>
