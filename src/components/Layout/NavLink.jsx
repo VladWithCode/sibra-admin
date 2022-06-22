29da3475efdc369f070d09b872d8a81f45db64b9
vladwithcode <vladwithcode@gmail.com>
2022-06-22 11:49:03 -0500

    Sidebar Navlink

---
+++ src/components/Layout/NavLink.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { NavLink as Link } from 'react-router-dom';
+
+function NavLink({ to, children, activeClassName = 'active' }) {
+  return (
+    <Link
+      to={to}
+      className={({ isActive }) =>
+        'link'.concat(isActive ? activeClassName : '').trim()
+      }>
+      {children}
+    </Link>
+  );
+}
+
+export default NavLink;
