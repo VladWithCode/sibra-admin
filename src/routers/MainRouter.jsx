3690475d5e2de776d71f9b780a2580807e01f6a0
vladwithcode <vladwithcode@gmail.com>
2022-06-22 10:09:11 -0500

    Browser => HashRouter

--- src/routers/MainRouter.jsx
+++ src/routers/MainRouter.jsx
@@ -1,16 +1,16 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import { App } from '../App';
 import Home from '../screens/Home';
 
 function MainRouter() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path='/' element={<App />}>
           <Route index element={<Home />} />
         </Route>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
