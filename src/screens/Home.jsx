e9321d05e2868f9281ac088e2ff37a3a35bc68a8
vladwithcode <vladwithcode@gmail.com>
2022-06-22 13:52:15 -0500

    Home Screen

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
 
 function Home() {
-  return <h1>Hello World!</h1>;
+  return (
+    <div className='homepage ui-screen'>
+      <div className='ui-screen__header'>
+        <h1 className='ui-screen__heading'>Inicio</h1>
+        <div className='ui-screen__menu'></div>
+      </div>
+      <div className='homepage__body ui-screen__body --flex'></div>
+    </div>
+  );
 }
 
 export default Home;
