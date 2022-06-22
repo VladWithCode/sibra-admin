304e7bfdd80623a461e0c8d0ed35ef1b9b628c64
vladwithcode <vladwithcode@gmail.com>
2022-06-22 13:52:25 -0500

    Error screen

---
+++ src/screens/Error.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Error() {
+  return (
+    <div className='ui-screen'>
+      <div className='ui-screen__header'>
+        <h1 className='ui-screen__heading'>Error!</h1>
+      </div>
+    </div>
+  );
+}
+
+export default Error;
