d2d48a745ad7674f2b018569aac8a969705a6eaa
vladwithcode <vladwithcode@gmail.com>
2022-06-23 16:10:38 -0500

    Project Details Route

--- src/routers/MainRouter.jsx
+++ src/routers/MainRouter.jsx
@@ -4,6 +4,7 @@ import Calendar from '../screens/Calendar/Calendar';
 import Customers from '../screens/Customers/Customers';
 import Error from '../screens/Error';
 import Home from '../screens/Home';
+import Project from '../screens/Projects/Project';
 import Projects from '../screens/Projects/Projects';
 import Templates from '../screens/Templates/Templates';
 
@@ -14,6 +15,7 @@ function MainRouter() {
         <Route path='/' element={<App />}>
           <Route index element={<Home />} />
           <Route path='/projects' element={<Projects />} />
+          <Route path='/projects/details/:id' element={<Project />} />
           <Route path='/customers' element={<Customers />} />
           <Route path='/calendar' element={<Calendar />} />
           <Route path='/templates' element={<Templates />} />
