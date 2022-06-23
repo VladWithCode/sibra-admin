46d1c72d464b2cea73c136ce0e422e77ebf8cca0
vladwithcode <vladwithcode@gmail.com>
2022-06-23 12:03:07 -0500

    Providers

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import NavigationProvider from './context/Navigation/NavigationProvider';
+import ProjectProvider from './context/Projects/ProjectProvider';
 import { SidebarProvider } from './context/Sidebar/SidebarProvider';
+import ToastProvider from './context/Toast/ToastProvider';
 import MainRouter from './routers/MainRouter';
 import './sass/main.scss';
 
@@ -9,10 +11,14 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <SidebarProvider>
-      <NavigationProvider>
-        <MainRouter />
-      </NavigationProvider>
-    </SidebarProvider>
+    <ToastProvider>
+      <SidebarProvider>
+        <NavigationProvider>
+          <ProjectProvider>
+            <MainRouter />
+          </ProjectProvider>
+        </NavigationProvider>
+      </SidebarProvider>
+    </ToastProvider>
   </React.StrictMode>
 );
