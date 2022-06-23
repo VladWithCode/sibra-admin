6085b0c13dff44dbe025343d3acf445b2ece191c
vladwithcode <vladwithcode@gmail.com>
2022-06-23 12:01:26 -0500

    Uppercase Convention

--- src/config/globals.js
+++ src/config/globals.js
@@ -1,5 +1,5 @@
-export const serverUrl = 'http://localhost:3000';
+export const SERVER_URL = 'http://localhost:3030';
 
-// export const serverUrl = 'http://192.168.1.128:3000'
+// export const SERVER_URL = 'http://192.168.1.128:3030'
 
-export const apiUrl = serverUrl + '/api';
+export const API_URL = SERVER_URL + '/api';
