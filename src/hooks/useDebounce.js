9ceb587409baa30a9a9e99439ca2c854c50e1297
vladwithcode <vladwithcode@gmail.com>
2022-06-23 12:16:47 -0500

    useDebounce

---
+++ src/hooks/useDebounce.js
@@ -0,0 +1,9 @@
+import { useEffect } from 'react';
+import { useTimeout } from './useTimeout';
+
+export const useDebounce = (cb, delay, deps) => {
+  const { reset, clear } = useTimeout(cb, delay);
+
+  useEffect(reset, [...deps, reset]);
+  useEffect(clear, []);
+};
