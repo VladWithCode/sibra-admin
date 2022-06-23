a8fba3b74aa61c2bf377a994d0c38337e7953a58
vladwithcode <vladwithcode@gmail.com>
2022-06-23 12:02:45 -0500

    Floating Menu

--- src/screens/Projects/Projects.jsx
+++ src/screens/Projects/Projects.jsx
@@ -1,10 +1,31 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
 import ScreenHeader from '../../components/Screen/ScreenHeader';
+import ProjectContext from '../../context/Projects/ProjectContext';
+import { useToast } from '../../hooks/useToast';
+import { getProjects } from '../../services/ProjectService';
+import ProjectMenu from './ProjectMenu';
 
 function Projects() {
+  const { projects, setProjects } = useContext(ProjectContext);
+  const { displayErrorToast } = useToast();
+
+  useEffect(() => {
+    const _getProjects = async () => {
+      const { projects, failed, toastMessage } = await getProjects();
+
+      if (failed) return displayErrorToast(toastMessage, true);
+
+      setProjects(projects);
+    };
+
+    if (!projects || projects.length === 0) _getProjects();
+  }, []);
+
   return (
     <div className='projectpage ui-screen'>
       <ScreenHeader heading='Proyectos'></ScreenHeader>
+
+      <ProjectMenu />
     </div>
   );
 }
