0c83e9b91c8a16f67ec9af05c1442458f23f45f0
vladwithcode <vladwithcode@gmail.com>
2022-06-23 12:02:59 -0500

    Toast

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Sidebar from './components/Layout/Sidebar';
+import Toast from './components/Toast/Toast';
 
 export const App = () => {
   return (
     <>
       <Sidebar />
       <Outlet />
+
+      <Toast />
     </>
   );
 };
