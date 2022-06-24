736a9c8caf5d9a343cf1eba03616f23818bcd3a0
vladwithcode <vladwithcode@gmail.com>
2022-06-24 12:07:41 -0500

    Use Custom Handler

--- src/components/UI/BackBtn.jsx
+++ src/components/UI/BackBtn.jsx
@@ -3,14 +3,19 @@ import FloatingBtn from '../Floating/FloatingBtn';
 import SVG from './SVG';
 import { useNavigate } from 'react-router-dom';
 
-function BackBtn({}) {
+function BackBtn({ onClick }) {
   const navigate = useNavigate();
+
+  const _onClick = e => {
+    typeof onClick === 'function' && onClick(e);
+    navigate(-1);
+  };
 
   return (
     <FloatingBtn
       className='btn--danger btn--round btn--back p-0'
       style={{ right: 'initial', left: '7rem' }}
-      onClick={() => navigate(-1)}>
+      onClick={_onClick}>
       <SVG id='angle' style={{ '--icon-sz': '4rem' }} />
     </FloatingBtn>
   );
