6193bc5597cb8b57690d337191fce1ba8491154d
vladwithcode <vladwithcode@gmail.com>
2022-06-30 13:06:19 -0500

    Change FileBtn file access logic

--- src/components/UI/FileBtn.jsx
+++ src/components/UI/FileBtn.jsx
@@ -1,15 +1,23 @@
 import React from 'react';
-import { SERVER_URL } from '../../config/globals';
+import { FILE_SERVER_URL, SERVER_URL } from '../../config/globals';
 import { getClassName } from '../../utils/helpers';
 import Button from './Button';
 
 function FileBtn({ children, path, className, onClick, ...props }) {
   const _onClick = e => {
     typeof onClick === 'function' && onClick(e);
+
     if (!props.forceCustomHandler) {
+      // Use default logic if not especified otherwise
       if (window.electronAPI) {
-        electronAPI.openFile(SERVER_URL + path);
+        // If on electron use main process to access file
+        const serverPath = String(path)
+          .replace('static', 'public')
+          .replace('/', '\\');
+
+        electronAPI.openFile(FILE_SERVER_URL + serverPath);
       } else {
+        // use regular method otherwise
         window.open(
           SERVER_URL + path,
           '_blank',
--- src/config/globals.js
+++ src/config/globals.js
@@ -1,5 +1,7 @@
-export const SERVER_URL = 'http://localhost:3030';
+// export const SERVER_URL = 'http://localhost:3030';
 
-// export const SERVER_URL = 'http://192.168.1.128:3030'
+export const SERVER_URL = 'http://192.168.1.128:3000';
 
 export const API_URL = SERVER_URL + '/api';
+
+export const FILE_SERVER_URL = '\\\\SIBRA-SERVER';
