bccb4632853b728333da6f0b1588752ececa5041
vladwithcode <vladwithcode@gmail.com>
2022-08-24 21:36:13 -0500

    [Change] Open files depending on path contents

--- public/listeners/file.js
+++ public/listeners/file.js
@@ -1,7 +1,13 @@
-const { ipcMain, shell } = require('electron');
+const { ipcMain, shell, BrowserWindow } = require('electron');
 
 ipcMain.on('app:open-file', (e, path) => {
-  shell.openExternal(path);
+  if (path.includes('localhost')) {
+    const w = new BrowserWindow({});
+
+    w.loadURL(path);
+  } else {
+    shell.openExternal(path);
+  }
 });
 
 ipcMain.on('app:browse-template', (e, _p) => {
