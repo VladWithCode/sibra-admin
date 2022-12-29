79159a561d1926777ea35e907fda02076c479e27
vladwithcode <vladwithcode@gmail.com>
2022-12-29 13:51:49 -0600

    Whoops

--- src/components/Toast/Toast.jsx
+++ src/components/Toast/Toast.jsx
@@ -1,19 +1,22 @@
 import React, { useContext } from 'react';
 import ToastContext from '../../context/Toast/ToastContext';
+import useToast from '../../hooks/context/toast/useToast';
+import { getClassName } from '../../utils/helpers';
 
 function Toast() {
-  const { type, message, visible, resetToast } = useContext(ToastContext);
+  const { message, type, isActive } = useToast((state) => state);
+  const { resetToast } = useContext(ToastContext);
 
-  const handleCloseClick = e => {
+  const handleCloseClick = (e) => {
     resetToast();
   };
 
   return (
-    <div className={`toast ${visible ? 'visible ' : ''}${type}`.trim()}>
-      <p className='toast__content'>{message}</p>
-      <span className='toast__close' onClick={handleCloseClick}>
+    <div className={getClassName('toast', isActive && 'visible', type)}>
+      <p className="toast__content">{message}</p>
+      <span className="toast__close" onClick={handleCloseClick}>
         <svg>
-          <use href='./assets/svg/plus.svg#plus'></use>
+          <use href="./assets/svg/plus.svg#plus"></use>
         </svg>
       </span>
     </div>
---
+++ src/utils/getClassname.js
@@ -0,0 +1,3 @@
+export default function getClassname(...args) {
+  args.filter(Boolean).join(' ');
+}
