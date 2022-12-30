9734093ce4056b7d156b15942897860ad660e9a8
vladwithcode <vladwithcode@gmail.com>
2022-12-30 12:09:49 -0600

    [Add] Nav, Projects and Sidebar Zustand Stores

---
+++ src/hooks/context/sidebar/useSidebar.js
@@ -0,0 +1,15 @@
+import create from 'zustand';
+
+const SIDEBAR_INITIAL_STATE = {
+  isExpanded: false,
+};
+
+const useSidebar = create((set) => ({
+  ...SIDEBAR_INITIAL_STATE,
+
+  setIsExpanded: (isExpanded) => set(() => ({ isExpanded: isExpanded })),
+
+  toggleIsExpanded: () => set((state) => ({ isExpanded: !state.isExpanded })),
+}));
+
+export default useSidebar;
