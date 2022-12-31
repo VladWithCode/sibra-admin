735b52cbdf7110fd252787592204162f4826f5c8
vladwithcode <vladwithcode@gmail.com>
2022-12-30 18:21:37 -0600

    [Add] Zustand Stores

---
+++ src/context/navigation/useNavigation.js
@@ -0,0 +1,24 @@
+import create from 'zustand';
+
+const INITIAL_NAVIGATION_STATE = {
+  projectsLink: '/projects',
+  customersLink: '/customers',
+  recordsLink: '/records',
+  calendarLink: '/calendar',
+  templatesLink: '/templates',
+};
+
+const useNavigation = create((set) => ({
+  ...INITIAL_NAVIGATION_STATE,
+
+  setLink: (name, value) =>
+    set(() => ({
+      [name]: value,
+    })),
+
+  resetLink: (name) => {
+    set(() => ({ [name]: INITIAL_NAVIGATION_STATE[name] }));
+  },
+}));
+
+export default useNavigation;
