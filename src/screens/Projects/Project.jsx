861feb61a8dab15c85f764b6717abdcd03a4d83e
vladwithcode <vladwithcode@gmail.com>
2022-12-30 18:24:41 -0600

    [Change] Use Zustand navigation store

--- src/screens/Projects/Project.jsx
+++ src/screens/Projects/Project.jsx
@@ -1,10 +1,9 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 import ProjectHeading from '../../components/Projects/ProjectHeading';
 import ScreenHeader from '../../components/Screen/ScreenHeader';
 import BackBtn from '../../components/UI/BackBtn';
-import NavigationContext from '../../context/Navigation/NavigationContext';
 import useProjects from '../../context/Projects/useProjects';
 import DetailsSection from '../../components/Projects/DetailsSection';
 import LotsSection from '../../components/Projects/LotsSection';
@@ -12,10 +11,11 @@ import StatSection from '../../components/Projects/StatSection';
 import FileSection from '../../components/Projects/FileSection';
 import ChargeSection from '../../components/Projects/ChargeSection';
 import Screen from '../../components/Screen/Screen';
+import useNavigation from '../../context/navigation/useNavigation';
 
 function Project() {
   const { pid } = useParams();
-  const { setLink, forceReset } = useContext(NavigationContext);
+  const resetLink = useNavigation((state) => state.resetLink);
   const { projects, project: index } = useProjects({ id: pid });
   const [project, setProject] = useState(projects[index]);
 
@@ -46,7 +46,7 @@ function Project() {
           <ChargeSection project={project} />
         </div>
       )}
-      <BackBtn to="/projects" onClick={() => forceReset('projects')} />
+      <BackBtn to="/projects" onClick={() => resetLink('projectsLink')} />
     </Screen>
   );
 }
