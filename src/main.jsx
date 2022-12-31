6f901eb33ac70ed4b8b93fb38e263ba3b0010f54
vladwithcode <vladwithcode@gmail.com>
2022-12-30 18:20:11 -0600

    [Add] React Query Client

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import ProjectProvider from './context/Projects/ProjectProvider';
+import { QueryClientProvider } from 'react-query';
+import queryClient from './queries/client';
 import MainRouter from './routers/MainRouter';
-// import './sass/main.scss';
 import './style.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <ProjectProvider>
+    <QueryClientProvider client={queryClient}>
       <MainRouter />
-    </ProjectProvider>
+    </QueryClientProvider>
   </React.StrictMode>
 );
