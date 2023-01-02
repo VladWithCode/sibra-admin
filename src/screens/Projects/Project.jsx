eb7785a87a78d385bf0b645f7962a71cc8b1e3aa
vladwithcode <vladwithcode@gmail.com>
2023-01-02 12:11:53 -0600

    [Change] Use React Query for state

--- src/screens/Projects/Project.jsx
+++ src/screens/Projects/Project.jsx
@@ -4,7 +4,6 @@ import { useParams } from 'react-router-dom';
 import ProjectHeading from '../../components/Projects/ProjectHeading';
 import ScreenHeader from '../../components/Screen/ScreenHeader';
 import BackBtn from '../../components/UI/BackBtn';
-import useProjects from '../../context/Projects/useProjects';
 import DetailsSection from '../../components/Projects/DetailsSection';
 import LotsSection from '../../components/Projects/LotsSection';
 import StatSection from '../../components/Projects/StatSection';
@@ -12,40 +11,40 @@ import FileSection from '../../components/Projects/FileSection';
 import ChargeSection from '../../components/Projects/ChargeSection';
 import Screen from '../../components/Screen/Screen';
 import useNavigation from '../../context/navigation/useNavigation';
+import { useGetProjectById } from '../../requests/projects/useProjectQueries';
+import ErrorScreen from '../../components/Screen/ErrorScreen';
+import LoadingScreen from '../../components/Screen/LoadingScreen';
 
 function Project() {
   const { pid } = useParams();
   const resetLink = useNavigation((state) => state.resetLink);
-  const { projects, project: index } = useProjects({ id: pid });
-  const [project, setProject] = useState(projects[index]);
+  const { isLoading, isError, data } = useGetProjectById(pid);
 
-  useEffect(() => {
-    if (!project && projects?.length > 0) {
-      setProject(projects.find((p) => p._id === pid));
-    }
-  }, [pid, projects, index]);
+  if (isError)
+    return (
+      <ErrorScreen
+        error={{
+          message: 'Ocurrio un error al recuperar el proyecto solicitado',
+        }}
+      />
+    );
+
+  if (isLoading) return <LoadingScreen />;
+
+  const { project } = data;
 
   return (
     <Screen>
       <ScreenHeader
-        heading={<ProjectHeading heading={project?.name} />}
+        heading={<ProjectHeading heading={project.name} />}
       ></ScreenHeader>
-      {(!project && (
-        <div
-          className="ui-screen__body d-f"
-          style={{ '--spin-sz': '7.5rem', '--spin-w': '1rem' }}
-        >
-          <div className="ui-spinner"></div>
-        </div>
-      )) || (
-        <div className="ui-screen__body project-details__body --scroll">
-          <DetailsSection project={project} />
-          <LotsSection project={project} />
-          {false && <StatSection project={project} />}
-          <FileSection project={project} />
-          <ChargeSection project={project} />
-        </div>
-      )}
+      <div className="ui-screen__body project-details__body --scroll">
+        <DetailsSection project={project} />
+        <LotsSection project={project} />
+        {false && <StatSection project={project} />}
+        <FileSection files={project.files} />
+        <ChargeSection projectId={project._id} charges={project.extraCharges} />
+      </div>
       <BackBtn to="/projects" onClick={() => resetLink('projectsLink')} />
     </Screen>
   );
--- src/screens/Projects/Projects.jsx
+++ src/screens/Projects/Projects.jsx
@@ -2,26 +2,33 @@ import React from 'react';
 import ProjectCard from '../../components/Projects/ProjectCard';
 import ScreenHeader from '../../components/Screen/ScreenHeader';
 import ProjectMenu from '../../components/Projects/ProjectMenu';
-import useProjects from '../../context/Projects/useProjects';
 import Screen from '../../components/Screen/Screen';
+import { useGetProjects } from '../../requests/projects/useProjectQueries';
+import ErrorScreen from '../../components/Screen/ErrorScreen';
+import LoadingScreen from '../../components/Screen/LoadingScreen';
 
 function Projects() {
-  const { projects, loading } = useProjects();
+  const { data, isLoading, isError } = useGetProjects({});
+
+  if (isError)
+    return (
+      <ErrorScreen
+        error={{
+          message: 'Ocurrio un error al recuperar el proyecto solicitado',
+        }}
+      />
+    );
+
+  if (isLoading) return <LoadingScreen />;
 
   return (
     <Screen>
-      <ScreenHeader heading='Proyectos'></ScreenHeader>
-      {loading ? (
-        <div className='d-f'>
-          <div className='ui-spinner'></div>
-        </div>
-      ) : (
-        <div className='projectpage__listing p-3'>
-          {projects.map(p => (
-            <ProjectCard project={p} key={p._id} />
-          ))}
-        </div>
-      )}
+      <ScreenHeader heading="Proyectos"></ScreenHeader>
+      <div className="projectpage__listing p-3">
+        {data.projects.map((p) => (
+          <ProjectCard project={p} key={p._id} />
+        ))}
+      </div>
       <ProjectMenu />
     </Screen>
   );
