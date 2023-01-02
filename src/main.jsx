160759b1c91baf9606f448c9d1e62bfb81cbd383
vladwithcode <vladwithcode@gmail.com>
2023-01-02 12:09:34 -0600

    [Change] rename to requests

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { QueryClientProvider } from 'react-query';
-import queryClient from './queries/client';
+import queryClient from './requests/client';
 import MainRouter from './routers/MainRouter';
 import './style.css';
 
