b4951beb63d94c89727ef0b5ba816d77dec0a9f3
vladwithcode <vladwithcode@gmail.com>
2023-01-03 13:08:54 -0600

    [Change] Move Spinner to component

--- src/components/Screen/LoadingScreen.jsx
+++ src/components/Screen/LoadingScreen.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
+import Spinner from '../UI/Spinner';
 import Screen from './Screen';
 import ScreenHeader from './ScreenHeader';
 
-function LoadingScreen({}) {
+function LoadingScreen({ children }) {
   return (
     <Screen>
-      <ScreenHeader heading="Cargando..." />
-      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-        <span className="block w-36 h-36 rounded-full border-8 border-b-info animate-spin" />
-      </div>
+      <ScreenHeader className="justify-between" heading="Cargando...">
+        {children}
+      </ScreenHeader>
+      <Spinner />
     </Screen>
   );
 }
