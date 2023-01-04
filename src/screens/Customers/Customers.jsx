5d16e2336ae710e69b150332a39fde113a8c0c7e
vladwithcode <vladwithcode@gmail.com>
2023-01-04 15:46:39 -0600

    [Change] Change nav link on customer click

--- src/screens/Customers/Customers.jsx
+++ src/screens/Customers/Customers.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import DebouncedInput from '../../components/Forms/DebouncedInput';
 import ErrorScreen from '../../components/Screen/ErrorScreen';
 import Screen from '../../components/Screen/Screen';
@@ -6,10 +7,12 @@ import ScreenBody from '../../components/Screen/ScreenBody';
 import ScreenHeader from '../../components/Screen/ScreenHeader';
 import Section from '../../components/Section/Section';
 import Spinner from '../../components/UI/Spinner';
+import useNavigation from '../../context/navigation/useNavigation';
 import { useGetCustomers } from '../../services/customers/useCustomers';
 
 function Customers() {
   const [search, setSearch] = useState('');
+  const setLink = useNavigation((state) => state.setLink);
   const { data, isLoading, isError, error } = useGetCustomers({
     name: search,
     sort: 'names',
@@ -28,23 +31,29 @@ function Customers() {
         {isLoading ? <Spinner /> : null}
         {data?.customers
           ? data.customers.map((customer) => (
-              <Section
-                headless={true}
-                useCard={true}
-                cardShadow={true}
-                bodyClass="flex flex-col py-6 h-[15rem] font-medium"
-                key={customer._id}
+              <Link
+                to={`/customers/${customer._id}`}
+                onClick={() =>
+                  setLink('customersLink', `/customers/${customer._id}`)
+                }
               >
-                <p className="text-xs text-zinc-300">{customer.rfc}</p>
-                <p className="text-xs text-zinc-300">{customer.curp}</p>
-                <p className="text-xl mt-auto">{customer.fullName}</p>
-                <p className="text-base text-zinc-500">
-                  {customer.phoneNumber}
-                </p>
-                {customer.email ? (
-                  <p className="text-sm text-zinc-400">{customer.email}</p>
-                ) : null}
-              </Section>
+                <Section
+                  headless={true}
+                  useCard={true}
+                  bodyClass="flex flex-col py-6 h-[15rem] font-medium shadow-zinc-200 shadow-md hover:shadow-lg active:shadow-sm"
+                  key={customer._id}
+                >
+                  <p className="text-xs text-zinc-300">{customer.rfc}</p>
+                  <p className="text-xs text-zinc-300">{customer.curp}</p>
+                  <p className="text-xl mt-auto">{customer.fullName}</p>
+                  <p className="text-base text-zinc-500">
+                    {customer.phoneNumber}
+                  </p>
+                  {customer.email ? (
+                    <p className="text-sm text-zinc-400">{customer.email}</p>
+                  ) : null}
+                </Section>
+              </Link>
             ))
           : null}
       </ScreenBody>
