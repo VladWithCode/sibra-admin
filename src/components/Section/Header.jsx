00f5db8684fe669ed195f138c65762b3795888a5
vladwithcode <vladwithcode@gmail.com>
2023-01-05 16:07:10 -0600

    [Change] Fix text classname

--- src/components/Section/Header.jsx
+++ src/components/Section/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Header({ heading, children }) {
   return (
     <div className="flex items-center justify-between mb-4">
-      {heading && <h3 className="texl-lg text-gray-500 px-1">{heading}</h3>}
+      {heading && <h3 className="text-lg text-gray-500 px-1">{heading}</h3>}
       {children}
     </div>
   );
