6b8ac4a06245ad5f3498a00ee4562d5203f67cfd
vladwithcode <vladwithcode@gmail.com>
2023-01-05 16:10:59 -0600

    [Change] use Card component

--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -1,21 +1,19 @@
 import React from 'react';
 import getClassname from '../../utils/getClassname';
+import Card from '../UI/Card';
 import Header from './Header';
 
 function Section({ className, heading, header, children, ...props }) {
   return (
     <div className={getClassname('h-max', className)}>
       {!props.headless ? <Header heading={heading}>{header}</Header> : null}
-      <div
-        className={getClassname(
-          'relative',
-          props.useCard && 'relative bg-white rounded-lg py-3 px-5',
-          props.cardShadow && 'shadow-md shadow-zinc-200',
-          props.bodyClass
-        )}
-      >
-        {children}
-      </div>
+      {props.useCard ? (
+        <Card className={props.bodyClass}>{children}</Card>
+      ) : (
+        <div className={getClassname('relative', props.bodyClass)}>
+          {children}
+        </div>
+      )}
     </div>
   );
 }
