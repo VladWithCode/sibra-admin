4a601640e9f0e7f16db37155ffececf1d2634947
vladwithcode <vladwithcode@gmail.com>
2023-03-21 14:45:31 -0600

    [Add] Short Lot Card

--- src/components/Lots/SlimLotCard.jsx
+++ src/components/Lots/SlimLotCard.jsx
@@ -1,7 +1,26 @@
 import React from 'react';
+import getClassname from '../../utils/getClassname';
+import Card from '../UI/Card';
 
-function SlimLotCard({}) {
-  return <div>SlimLotCard</div>;
+function SlimLotCard({ lot, className }) {
+  return (
+    <Card className={getClassname(className)}>
+      <div className="flex justify-between">
+        <p className="text-xl font-bold basis-1/2">
+          Lote {lot.lotNumber} Manzana {lot.manzana}
+        </p>
+        {lot.customer ? (
+          <p className="text-lg basis-1/2 text-right font-medium">
+            {lot.customer.name}
+          </p>
+        ) : (
+          <p className="text-lg text-blue-500 text-right font-medium">
+            Disponible
+          </p>
+        )}
+      </div>
+    </Card>
+  );
 }
 
 export default SlimLotCard;
