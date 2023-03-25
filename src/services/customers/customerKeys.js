791d2c17f4d4be83059c0c4a09be1da27b0e3a88
vladwithcode <vladwithcode@gmail.com>
2023-03-25 13:49:53 -0600

    [Add] Customer file keys

--- src/services/customers/customerKeys.js
+++ src/services/customers/customerKeys.js
@@ -1,9 +1,11 @@
 const customerKeys = {
   all: () => ['customers'],
   lists: () => [...customerKeys.all(), 'list'],
-  list: (filters) => [...customerKeys.lists(), { ...filters }],
+  list: filters => [...customerKeys.lists(), { ...filters }],
   details: () => [...customerKeys.all(), 'detail'],
-  detail: (id) => [...customerKeys.details(), id],
+  detail: id => [...customerKeys.details(), id],
+  files: () => [...customerKeys.all, 'file'],
+  fileDetail: fileId => [...customerKeys.files(), file],
 };
 
 export default customerKeys;
