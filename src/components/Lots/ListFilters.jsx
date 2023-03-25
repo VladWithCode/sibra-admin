ca9081fb5b9c31b1e2b8f686020e43f0c4512444
vladwithcode <vladwithcode@gmail.com>
2023-03-25 13:48:04 -0600

    [Add] Lot filtering controls

---
+++ src/components/Lots/ListFilters.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Card from '../UI/Card';
+
+function ListFilters() {
+  return (
+    <Card className="max-h-screen space-y-4 --scroll">
+      <p className="text-xl font-bold mb-4">Filtrar por...</p>
+      
+    </Card>
+  );
+}
+
+export default ListFilters;
