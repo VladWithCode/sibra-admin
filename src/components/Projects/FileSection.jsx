2f686c44e7455879c2f2720743e3da9fb1f3efb0
vladwithcode <vladwithcode@gmail.com>
2023-03-25 13:48:43 -0600

    [Add] No file feedback

--- src/components/Projects/FileSection.jsx
+++ src/components/Projects/FileSection.jsx
@@ -13,7 +13,12 @@ function FileSection({ files }) {
       cardShadow={true}
     >
       <Row className="gap-x-4">
-        {files.map((f) => {
+        {files.length === 0 ? (
+          <p className="text-2xl text-zinc-300 text-center font-semibold w-full py-4">
+            Sin archivos
+          </p>
+        ) : null}
+        {files.map(f => {
           return (
             <div className="basis-auto grow-0 shrink-0" key={f._id}>
               <FileBtn path={f.staticPath}>{f.name}</FileBtn>
