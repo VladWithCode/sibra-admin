df55431585a44c05bb1e5cf5abe156469cf9a2fd
vladwithcode <vladwithcode@gmail.com>
2023-03-25 13:50:14 -0600

    [Add] Put File service

--- src/services/customers/useCustomers.js
+++ src/services/customers/useCustomers.js
@@ -1,4 +1,5 @@
-import { useQuery } from 'react-query';
+import { useMutation, useQuery } from 'react-query';
+import { API_URL } from '../../config/globals';
 import customerKeys from './customerKeys';
 import { fetchCustomers } from './fetchers';
 
@@ -6,6 +7,30 @@ export const useGetCustomers = ({ name, project, sort }) => {
   return useQuery(customerKeys.list({ name, project, sort }), fetchCustomers);
 };
 
-export const useGetCustomerById = (id) => {
+export const useGetCustomerById = id => {
   return useQuery(customerKeys.detail(id), fetchCustomers);
+};
+
+/**
+ * File upload service
+ */
+export const usePutFile = ({ customerId, fileName, file }) => {
+  const mutateFn = async () => {
+    const fd = new FormData();
+
+    fd.append('file', file);
+    fd.append('fileName', fileName);
+
+    const response = await fetch(API_URL + `/customers/${customerId}/file`, {
+      method: 'PUT',
+      body: fd,
+    });
+    const data = await response.json();
+
+    if (!response.ok) throw new Error(data.message);
+
+    return data;
+  };
+
+  return useMutation(customerKeys.files(), mutateFn);
 };
